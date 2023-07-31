549ed6230fedb5fe7869c19ca4bf7365c7b41a58
leojuriolli7 <leojuriolli@gmail.com>
2023-07-31 14:09:37 -0300

    fix: ios create chat layout

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -46,7 +46,7 @@ const CommandInput = React.forwardRef<
     <CommandPrimitive.Input
       ref={ref}
       className={cn(
-        "flex h-10 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-neutral-500 disabled:cursor-not-allowed disabled:opacity-50 dark:placeholder:text-neutral-400",
+        "flex h-10 w-full rounded-md bg-transparent py-3 md:text-sm outline-none placeholder:text-neutral-500 disabled:cursor-not-allowed disabled:opacity-50 dark:placeholder:text-neutral-400",
         className
       )}
       {...props}
